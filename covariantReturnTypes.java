466e7aebaeeb44bd5d8c528d5fdcc01b0ebf77d7
HebaBeshai <hebabeshai@yahoo.com>
2020-09-09 13:54:16 -0400

    Update covariantReturnTypes.java

--- covariantReturnTypes.java
+++ covariantReturnTypes.java
@@ -1,4 +1,6 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 //Complete the classes below
 class Flower {
@@ -45,3 +47,21 @@ class AndhraPradesh extends Region {
     }
 }
 
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      String s = reader.readLine().trim();
+      Region region = null;
+      switch (s) {
+        case "WestBengal":
+          region = new WestBengal();
+          break;
+        case "AndhraPradesh":
+          region = new AndhraPradesh();
+          break;
+      }
+      Flower flower = region.yourNationalFlower();
+      System.out.println(flower.whatsYourName());
+    }
+}
